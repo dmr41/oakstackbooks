29c30b90748dcb941f02b86aea84bf890639de52
David Rivers <dmr41.rivers@gmail.com>
2015-09-20 15:16:53 -0600

    removed cruft from intial fiddling. Base request object in place and functioning

--- README.md
+++ README.md
@@ -1,11 +1,20 @@
-# oakstackbooks
+# Oakstack Books
 
-- Project restarted 09/19/2105 with heavier Javascript frontend functionality.
-- Goal is to make an app that:
-    - Stores users personal book data
-    - Using the Biblio.com API for Real-time evaluation of value
-    - Book value based off:
-      1. Current sellers pricing
-      2. Book Condition
-      3. Edition
-      4. Signatures
+### Development info
+* Project restarted 09/19/2105
+* Ruby/Rails approach scrapped - 
+* New approach
+  * Javascript API request
+  * Javascript/HTML5 frontend logic heavy
+  * Plan to use MongoDB
+
+### Key Goals
+* Stores users personal book data
+* Using the Biblio.com API for Real-time evaluation of value
+
+### ![Biblio image icon](https://d3525k1ryd2155.cloudfront.net/i/en/n/b_icon_30x30.png) Biblio.com API
+- Book value based off:
+  * Current sellers pricing
+  * Book Condition
+  * Edition
+  * Signatures
--- biblio_request.js
+++ biblio_request.js
@@ -3,12 +3,15 @@ var https = require('https');
 var qs = require('querystring');
 var url = 'www.biblio.com'
 var searchPath = '/api/product_search';
-var query_string = qs.stringify({ "author": 'chad harbach', 'results_per_page': '199' }) // 199 is api limit per page
+var query_string = qs.stringify({ 'author': 'chad harbach',
+                                  'results_per_page': '50',
+                                  'minimum_price':'10',
+                                  'page': '3' }
+                                ); // 199 is api limit per page
 var fullPath = searchPath+"?"+query_string;
-// var bparams = "?author=chad+harbach&title=art+of+fielding&ship_to_country=USA&currency=USD&sort_field=price&sort_direction=desc&minimum_price=10&results_per_page=100";
 var options = {
   hostname: url,
-  headers: {'X-API-KEY': process.env.USER_KEY},
+  headers: {'X-API-KEY': process.env.USER_KEY}, //set local env var USER_KEY
   path: fullPath,
   method: 'GET',
   json: true
